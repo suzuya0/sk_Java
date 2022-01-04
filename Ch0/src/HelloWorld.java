ed204e669f6cff1328f3d8abcedc16282e5f2cff
suzuya0 <sakura240613@gmail.com>
2022-01-04 22:49:20 +0900

    [Finish]Ch0

---
+++ Ch0/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello World");
+		System.out.println(31+31);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
