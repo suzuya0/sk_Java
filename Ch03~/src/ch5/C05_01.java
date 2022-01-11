441f46cc8070c99d53cc542953e2884e11a613f8
suzuya0 <sakura240613@gmail.com>
2022-01-11 18:14:08 +0900

    [Finish]Ch05-01

---
+++ Ch03~/src/ch5/C05_01.java
@@ -0,0 +1,7 @@
+package ch5;
+
+public class C05_01 {
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
+}
---
+++ Ch03~/src/ch5/C05_03.java
@@ -0,0 +1,17 @@
+package ch5;
+
+public class C05_03 {
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+	
+	public static void main(String[] args) {
+		methodA();
+	}
+
+}
