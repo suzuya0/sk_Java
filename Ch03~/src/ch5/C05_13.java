02d84835c875584696118d98ee9a2c6e842a10d5
suzuya0 <sakura240613@gmail.com>
2022-01-15 23:56:33 +0900

    [Finish]Ch05-5

---
+++ Ch03~/src/ch5/C05_13.java
@@ -0,0 +1,17 @@
+package ch5;
+
+public class C05_13 {
+	public static void incArray(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			array[i]++;
+		}
+	}
+	
+	public static void main (String[] args){
+		int[] array = {1, 2, 3};
+		incArray(array);
+		for(int i: array) {
+			System.out.println(i);
+		}
+	}
+}
