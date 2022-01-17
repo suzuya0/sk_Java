ffc13740f81d2a5540a71fa34443aea94d146cc9
suzuya0 <sakura240613@gmail.com>
2022-01-17 18:13:29 +0900

    [Finish]Ch06-01

---
+++ Ch03~/src/ch6/C06_02.java
@@ -0,0 +1,12 @@
+package ch6;
+
+public class C06_02 {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+
+}
---
+++ Ch03~/src/ch6/C06_04.java
@@ -0,0 +1,11 @@
+package ch6;
+
+public class C06_04 {
+	public static void main(String[] args) {
+		int a = 10; int b = 2;
+		int total = C06_02.tasu(a, b);
+		int delta = C06_02.hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+
+}
