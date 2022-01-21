36437d7daba9890ea5349966d01452a10abdd199
suzuya0 <sakura240613@gmail.com>
2022-01-21 23:08:24 +0900

    [Finish]Ch06-06

---
+++ Ch03~/src/ch6/C06_10.java
@@ -0,0 +1,12 @@
+package ch6;
+
+public class C06_10 {
+	public static void main(String[] args) {
+		int[] heights = {172, 149, 152, 191, 155};
+		java.util.Arrays.sort(heights);
+		for(int h: heights) {
+			System.out.println(h);
+		}
+	}
+
+}
--- Ch03~/src/ch6/main/C06_08.java
+++ Ch03~/src/ch6/main/C06_08.java
@@ -1,5 +1,18 @@
 package ch6.main;
 
-public class C06_08 {
+//クラスを指定してインポート
+import ch6.logics.C06_06;
 
+//ch06.logicの全てをインポート
+//import ch6.logics.*;
+
+public class C06_08 {
+	public static void main(String[] args) {
+		int a = 10; int b = 2;
+		int total = C06_06.tasu(a, b);
+		int delta = C06_06.hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+		
+	}
+	
 }
